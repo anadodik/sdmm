40d1974f9d79b8aa5073af852aebf9321ff87a79
Ana Dodik <anadodik94@gmail.com>
2020-07-09 12:27:55 +0200

    run comparisons multiple times

--- tests/comparison.cpp
+++ tests/comparison.cpp
@@ -132,69 +132,70 @@ TEST_CASE("sampling/pdf comparison") {
     >;
     using MMCond = typename MM::ConditionalDistribution;
 
-    auto jmm_distribution = MM();
-    auto conditional = MMCond();
-    jmm_distribution.setNComponents(t_components);
-    conditional.setNComponents(t_components);
-    for(int i = 0; i < t_components; ++i) {
-        MM::Vectord mean;
-        mean << 
-            init_rng.next_float32(),
-            init_rng.next_float32(),
-            init_rng.next_float32(),
-            init_rng.next_float32(),
-            init_rng.next_float32(),
-            init_rng.next_float32();
-        mean.bottomRows(3) /= mean.bottomRows(3).norm();
-        jmm_distribution.weights()[i] = 1.f / Scalar(t_components);
-        jmm_distribution.components()[i].set(
-            mean,
-            MM::Matrixd::Identity()
+    for(int ctr = 0; ctr < 5; ++ctr) {
+        auto jmm_distribution = MM();
+        auto conditional = MMCond();
+        jmm_distribution.setNComponents(t_components);
+        conditional.setNComponents(t_components);
+        for(int i = 0; i < t_components; ++i) {
+            MM::Vectord mean;
+            mean << 
+                init_rng.next_float32(),
+                init_rng.next_float32(),
+                init_rng.next_float32(),
+                init_rng.next_float32(),
+                init_rng.next_float32(),
+                init_rng.next_float32();
+            mean.bottomRows(3) /= mean.bottomRows(3).norm();
+            jmm_distribution.weights()[i] = 1.f / Scalar(t_components);
+            jmm_distribution.components()[i].set(
+                mean,
+                MM::Matrixd::Identity()
+            );
+        }
+        jmm_distribution.configure();
+
+        typename MM::ConditionVectord jmm_point({1, 1, 1});
+        Scalar heuristicWeight;
+        
+        jmm_distribution.conditional(jmm_point, conditional, heuristicWeight);
+        auto jmm_sample = conditional.sample(
+            [&jmm_rng]() mutable -> Scalar { return jmm_rng.next_float32().coeff(0); }
         );
+
+        Scalar jmm_pdf = conditional.pdf(jmm_sample);
+        spdlog::info("jmm_sample={}", jmm_sample.transpose());
+        spdlog::info("jmm_pdf={}", jmm_pdf);
+
+        #endif // COMPARISON == 1
+
+        JointSDMM distribution;
+        copy_sdmm(jmm_distribution, distribution);
+
+        Conditioner conditioner;
+        enoki::set_slices(conditioner, enoki::slices(distribution));
+        sdmm::prepare(conditioner, distribution);
+
+        sdmm::embedded_s_t<MarginalSDMM> point({1, 1, 1});
+        sdmm::create_conditional(conditioner, point);
+
+        Value inv_jacobian;
+        enoki::set_slices(inv_jacobian, enoki::slices(distribution));
+        sdmm::replace_embedded_t<ConditionalSDMM, Value> sample;
+        sdmm::replace_tangent_t<ConditionalSDMM, Value> tangent_sample;
+        
+        conditioner.conditional.sample(rng, sample, inv_jacobian, tangent_sample);
+        spdlog::info("sample={}, norm={}", sample, enoki::norm(sample));
+
+        Value posterior;
+        enoki::set_slices(posterior, enoki::slices(distribution));
+        enoki::vectorize_safe(
+            VECTORIZE_WRAP_MEMBER(posterior),
+            conditioner.conditional,
+            sample,
+            posterior
+        );
+        float pdf = enoki::hsum_nested(posterior);
+        spdlog::info("pdf={}", pdf);
     }
-    jmm_distribution.configure();
-
-    typename MM::ConditionVectord jmm_point({1, 1, 1});
-    Scalar heuristicWeight;
-    
-    jmm_distribution.conditional(jmm_point, conditional, heuristicWeight);
-    auto jmm_sample = conditional.sample(
-        [&jmm_rng]() mutable -> Scalar { return jmm_rng.next_float32().coeff(0); }
-    );
-
-    Scalar jmm_pdf = conditional.pdf(jmm_sample);
-    spdlog::info("jmm_sample={}", jmm_sample.transpose());
-    spdlog::info("jmm_pdf={}", jmm_pdf);
-
-    #endif // COMPARISON == 1
-
-    JointSDMM distribution;
-    copy_sdmm(jmm_distribution, distribution);
-
-    Conditioner conditioner;
-    enoki::set_slices(conditioner, enoki::slices(distribution));
-    sdmm::prepare(conditioner, distribution);
-
-    sdmm::embedded_s_t<MarginalSDMM> point({1, 1, 1});
-    sdmm::create_conditional(conditioner, point);
-
-    Value inv_jacobian;
-    enoki::set_slices(inv_jacobian, enoki::slices(distribution));
-    sdmm::replace_embedded_t<ConditionalSDMM, Value> sample;
-    sdmm::replace_tangent_t<ConditionalSDMM, Value> tangent_sample;
-    
-    conditioner.conditional.sample(rng, sample, inv_jacobian, tangent_sample);
-    spdlog::info("sample={}", sample);
-    spdlog::info("inv_jacobian={}", inv_jacobian);
-
-    Value posterior;
-    enoki::set_slices(posterior, enoki::slices(distribution));
-    enoki::vectorize_safe(
-        VECTORIZE_WRAP_MEMBER(posterior),
-        conditioner.conditional,
-        sample,
-        posterior
-    );
-    float pdf = enoki::hsum_nested(posterior);
-    spdlog::info("pdf={}", pdf);
 }
